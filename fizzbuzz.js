ca05a6efa2de34cdb3a7568d129ba17a41a4e62b
kbgrilling <32076202+kbgrilling@users.noreply.github.com>
2021-08-18 23:45:36 -0400

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,47 +1,46 @@
 // javascript file
 
 function fizzbuzz(firstNum, secondNum) {
-
+    // loop through 1 through 100
     for (let i = 1; i < 101; i++) {
-        //let fizzbuzzListItem = document.createElement('li');
-        //fizzbuzzList.appendChild(fizzbuzzListItem);
+        // create a div element to hold the current loop number
         let fizzbuzzElement = document.createElement('div');
+
         let countString = String(i);
-        //console.log(i);
+
+        // updates the h3 tag in the middle two grid columns with the numbers selected by the user.;
         document.getElementById('onlyBuzz').getElementsByTagName('h3')[0].innerHTML = secondNum + " brings the BUZZ";
         document.getElementById('onlyFizz').getElementsByTagName('h3')[0].innerHTML = firstNum + " brings the FIZZ";
 
+        //  if both numbers are multiples of current loop number add to the FIZZBUZZ column;
         if  (i % firstNum == 0 && i % secondNum == 0) {
             countString = countString + '- YES WE HAVE FIZZBUZZ';
             let anyFizzbuzz = document.createTextNode(countString);
             fizzbuzzElement.appendChild(anyFizzbuzz);
             document.getElementById('fizzbuzz').appendChild(fizzbuzzElement);
-           // console.log('fizzbuzz');
-        } else if (i % secondNum == 0) {
 
+        } else if (i % secondNum == 0) {
+            // if second number is a multiple of loop number add to buzz column;
             countString = countString + ': Only buzz';
             let anyFizzbuzz = document.createTextNode(countString);
             fizzbuzzElement.appendChild(anyFizzbuzz);
             document.getElementById('onlyBuzz').appendChild(fizzbuzzElement);
 
-            //console.log('buzz');
         } else if (i % firstNum == 0) {
+            // if first number is a multiple of loop number add to fizz column;
             countString = countString + ':  Only fizz';
             let anyFizzbuzz = document.createTextNode(countString);
             fizzbuzzElement.appendChild(anyFizzbuzz)
             document.getElementById('onlyFizz').appendChild(fizzbuzzElement);
-            //fizzbuzzListItem.appendChild(itemNum);
-            //console.log('fizz');
+
         } else {
+            // if either number is not a mulitple of loop number add to nothing column.;
             countString = countString + ': ah man, Nothing!'
             let anyFizzbuzz = document.createTextNode(countString);
             fizzbuzzElement.appendChild(anyFizzbuzz)
             document.getElementById('noFizzbuzz').appendChild(fizzbuzzElement);
         }
-
-        //let itemNum = document.createTextNode(countString);
-       // fizzbuzzListItem.appendChild(itemNum);
-       //document.getElementById('printFizzBuzz').appendChild(fizzbuzzList);
+        // turns on visibility of the result grid;
         document.getElementById("results").style.visibility = 'visible';
     }
 }
